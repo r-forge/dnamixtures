7bdfe5fc71e1926c2f0fbe0f6ca337210e629e4d
therese (Therese Graversen) <theg@itu.dk>
2023-06-19 11:51:28 +0000

    Added DNAmixturesLite downloads badge.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dnamixtures@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/DNAmixturesLite.php
+++ www/DNAmixturesLite.php
@@ -1,5 +1,7 @@
 <h1>DNAmixturesLite</h1>
 
+<p><a href="https://cran.r-project.org/package=DNAmixturesLite"><img src="http://cranlogs.r-pkg.org/badges/grand-total/DNAmixturesLite?color=blue" alt=""></a></p>
+
 <p>
 <span class="rpkg">DNAmixturesLite</span> is developed to enable users, who do not have a commercial license for Hugin, to experiment with the methodology provided in <span class="rpkg">DNAmixtures</span>. 
 </p>
